35a6ce5e820548baaeb7ab06612b2184067fadda
g6bby <nabag123@newschool.edu>
2023-09-15 18:12:42 -0400

    added UI and raycast for pressing E to start convo

---
+++ Assets/Scripts/ObjectInteraction.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ObjectInteraction : MonoBehaviour
+{
+    public GameObject talkText;
+    private bool talkActive;
+    
+    public float interactionDistance = 2f; 
+
+
+    void Start()
+    {
+        talkActive = false;
+    }
+
+    void Update()
+    {
+
+        if (talkActive == false)
+        {
+            talkText.SetActive(false);
+        }
+        else
+        {
+            talkText.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, interactionDistance))
+            {
+                InteractableObject interactableObject = hit.collider.GetComponent<InteractableObject>();
+                
+                if (interactableObject != null)
+                {
+                    interactableObject.Interact();
+                }
+            }
+        }
+
+        if (talkActive == false)
+        {
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            
+            if (Physics.Raycast(ray, out hit, interactionDistance))
+            {
+                InteractableObject interactableObject = hit.collider.GetComponent<InteractableObject>();
+                
+                if (interactableObject != null)
+                {
+                    talkText.SetActive(true);
+                }
+            }
+        }
+    }
+}
